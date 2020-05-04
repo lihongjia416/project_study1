7bca241d724dbccc55682e3bef59e0c484003507
tom_pro <goodMoring@atguigu.com>
2020-05-04 19:10:54 +0800

    conflect

--- src/happy.java
+++ src/happy.java
@@ -1,12 +1,8 @@
 
 public class happy {
-
 	public happy() {
-<<<<<<< HEAD
 		System.out.println("right");
-=======
 		System.out.println("left");
->>>>>>> branch 'master' of https://github.com/lihongjia416/project_study1.git
 	}
 
 }
